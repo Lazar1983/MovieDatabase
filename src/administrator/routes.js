9ac535138141593a3158fa9fc8adc7a9f289ad48
Lazar <lazov_lazar@yahoo.com>
2019-08-01 12:17:34 +0200

    admin create movie

--- src/administrator/routes.js
+++ src/administrator/routes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import actions from './actions';
+import actionsMovies from '../movies/actions';
 
 const { list, create } = actions;
+const { createMovie } = actionsMovies;
  
 const adminRouter = Router();
 
 adminRouter.get('/admin', list);
 adminRouter.post('/admin', create);
-// adminRouter.post('/admin/:id/movies', createMovie);
+adminRouter.post('/admin/:id/movies', createMovie);
 
 
 
